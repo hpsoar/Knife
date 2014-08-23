9a4d5e130eba3979bf454d4de1f7c79c3a585435
hpsoar <hpsoar@gmail.com>
2014-08-23 17:28:56 +0800

    Initial Commit

---
+++ Knife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knife
+//
+//  Created by HuangPeng on 8/23/14.
+//  Copyright (c) 2014 Beacon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
